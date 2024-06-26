7d154f906fdc6ba66fa9c50a6acca30b6cc35e71
NEHA M SIBI <116068235+nehamsibi@users.noreply.github.com>
2024-06-26 15:21:08 +0530

    counter_bloc.dart

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -5,18 +5,6 @@ import 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   final CounterRepository repository;
-//   CounterBloc({required this.repository}) : super(CounterInitial()) {
-//     on<LoadCounter>((event, emit) async {
-//       emit(CounterLoading());
-//       try {
-//         final counter = await repository.fetchCounter();
-//         emit(CounterLoaded(counter));
-//       } catch (e) {
-//         emit(CounterError("Failed to load counter"));
-//       }
-//     });
-// final CounterRepository repository;
-
   CounterBloc({required this.repository}) : super(CounterInitial()) {
     on<LoadCounter>((event, emit) async {
       emit(CounterLoading());
@@ -31,18 +19,7 @@ class CounterBloc extends Bloc<CounterEvent, CounterState> {
         emit(CounterError("Failed to load counter: $e"));
       }
     });
-//}
 
-    // on<IncrementCounter>((event, emit) async {
-    //   if (state is CounterLoaded) {
-    //     final currentState = state as CounterLoaded;
-    //     final newValue = currentState.counter.value + 1;
-    //     if (newValue <= currentState.counter.maxValue) {
-    //       await repository.updateCounter(newValue);
-    //       emit(CounterLoaded(currentState.counter.counternew(value: newValue)));
-    //     }
-    //   }
-    // });
     on<IncrementCounter>((event, emit) async {
       if (state is CounterLoaded) {
         final currentState = state as CounterLoaded;
